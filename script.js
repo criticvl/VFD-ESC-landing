5623f8f01210710e9068c50e99a12b3c07ae2b8f
criticvl <beso.urekadze.1@btu.edu.ge>
2023-05-22 08:55:10 +0400

    Update 9

---
+++ script.js
@@ -0,0 +1,47 @@
+const navButtons = [...document.getElementsByClassName("navButton")];
+const footerNavButtons = [
+  ...document.getElementsByClassName("footerNavButton"),
+];
+
+const allNavButtons = navButtons.concat(footerNavButtons);
+
+allNavButtons.forEach((item) => {
+  item.addEventListener("click", () => {
+    navButtons.forEach((i) => {
+      i.classList.remove("active");
+      if (i.getAttribute("href") == item.getAttribute("href")) {
+        i.classList.add("active");
+      }
+    });
+  });
+});
+
+const clipboardButtons = [...document.getElementsByClassName("clipboard")];
+
+clipboardButtons.forEach((item) => {
+  item.addEventListener("click", () => {
+    navigator.clipboard.writeText(item.textContent).then(
+      function () {
+        swal("Email was copied to Clipboard!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  });
+});
+
+const emailSubForm = document.getElementById("emailSubForm");
+const emailSubButton = document.getElementById("emailSubButton");
+emailSubButton.addEventListener("click", (e) => {
+  if (!emailSubForm.checkValidity()) {
+    var tmpSubmit = document.createElement("button");
+    emailSubForm.appendChild(tmpSubmit);
+    tmpSubmit.click();
+    emailSubForm.removeChild(tmpSubmit);
+  } else {
+    swal("You have successfully subscribed to our Newsletter!");
+    emailSubForm.reset();
+  }
+  e.preventDefault();
+});
